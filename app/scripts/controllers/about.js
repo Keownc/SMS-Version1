4ba02c41b6954cd64386440562e98a882436b311
Keown Creese <kcreese@fullsail.edu>
2016-03-09 22:08:24 -0500

    Started a new project file

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name workPunchApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the workPunchApp
+ */
+angular.module('workPunchApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name workPunchApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the workPunchApp
+ */
+angular.module('workPunchApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
