cdec7538ffd17aa85868cab9f34655c232227bbc
Keown Creese <kcreese@fullsail.edu>
2016-03-15 17:12:11 -0400

    Added firebase with bower

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -8,10 +8,38 @@
  * Controller of the workPunchApp
  */
 angular.module('workPunchApp')
-  .controller('HomeCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('HomeCtrl', function ($scope, $firebaseArray) {
+    //   this.awesomeThings = [
+    //     'HTML5 Boilerplate',
+    //     'AngularJS',
+    //     'Firebase',
+    //     'Karma'
+    //     ];
+    //     var Firebase;
+      var URL = 'https://workpunchdev.firebaseio.com/';
+      var list = $firebaseArray(new Firebase(URL));
+
+      list.$loaded( function(data) {
+        console.log('Data', data);
+        $scope.users = data;
+      }, function(error) {
+        console.error('Error:', error);
+      });
+
+      // add an item
+      //list.$add({ foo: "bar" }).then(...);
+
+      // remove an item
+      //list.$remove(2).then(...);
+
+      // make the list available in the DOM
+      $scope.users = list;
+
+      $scope.addName = function() {
+          list.$add($scope.name).then(function(ref) {
+              var id = ref.key();
+              console.log('added record with id ' + id);
+              list.$indexFor(id); // returns location in the array
+          });
+      };
   });
